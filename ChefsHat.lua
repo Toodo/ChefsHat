d31524bfd7fb537fdeb01677d1fdbbf0373370b0
Toodo <kapten_todo@msn.com>
2025-04-23 02:00:13 +0200

    Update ChefsHat.lua
    Update: Chef's Hat will now be equipped after casting Cooking Fire, if the Cooking skill window is opened at the same time.

--- ChefsHat.lua
+++ ChefsHat.lua
@@ -3,28 +3,59 @@ local f = CreateFrame("Frame")
 local chefsHatId = 46349
 local previousHelmId
 
+local isCastingCookingFire = false
+local cookingWindowOpened = false
+
 local function hasCookingProfession()
     local skillLine = GetTradeSkillLine()
     return skillLine and skillLine == "Cooking"
 end
 
-local function onEvent(self, event, ...)
+local function EquipChefsHatIfNeeded()
+    local helmSlot = GetInventoryItemID("player", 1)
+    if helmSlot and helmSlot ~= chefsHatId then
+        previousHelmId = helmSlot
+        EquipItemByName(chefsHatId)
+    end
+end
+
+local function RestorePreviousHelm()
+    if previousHelmId then
+        EquipItemByName(previousHelmId)
+        previousHelmId = nil
+    end
+end
+
+local function onEvent(self, event, arg1, arg2)
     if event == "TRADE_SKILL_SHOW" then
-        if hasCookingProfession() then
-            local helmSlot = GetInventoryItemID("player", 1)
-            if helmSlot and helmSlot ~= chefsHatId then
-                previousHelmId = helmSlot
-                EquipItemByName(chefsHatId)
-            end
+        cookingWindowOpened = true
+        if not isCastingCookingFire and hasCookingProfession() then
+            EquipChefsHatIfNeeded()
         end
+
     elseif event == "TRADE_SKILL_CLOSE" then
-        if previousHelmId then
-            EquipItemByName(previousHelmId)
-            previousHelmId = nil
+        cookingWindowOpened = false
+        RestorePreviousHelm()
+
+    elseif (event == "UNIT_SPELLCAST_START" or event == "UNIT_SPELLCAST_CHANNEL_START") and arg1 == "player" then
+        local spellName = UnitCastingInfo("player")
+        if spellName == GetSpellInfo(818) then
+            isCastingCookingFire = true
+        end
+
+    elseif event == "UNIT_SPELLCAST_STOP" and arg1 == "player" then
+        if isCastingCookingFire then
+            isCastingCookingFire = false
+            if cookingWindowOpened and hasCookingProfession() then
+                C_Timer.After(0.1, EquipChefsHatIfNeeded)
+            end
         end
     end
 end
 
 f:RegisterEvent("TRADE_SKILL_SHOW")
 f:RegisterEvent("TRADE_SKILL_CLOSE")
+f:RegisterEvent("UNIT_SPELLCAST_START")
+f:RegisterEvent("UNIT_SPELLCAST_STOP")
+f:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START")
 f:SetScript("OnEvent", onEvent)
